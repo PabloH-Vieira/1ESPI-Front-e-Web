b3120b94fd164c711c75ab6347fffdefda077974
Pablo <pabloallmeida123@gmail.com>
2024-03-21 08:54:07 -0300

    Declarando variáveis do tipo NULL, vazia e com val

---
+++ script.js
@@ -0,0 +1,21 @@
+let nome;
+console.log(nome);
+let disciplina = null;
+console.log(disciplina);
+//Exibindo no console
+let aluno = "FIAP";
+console.log(aluno);
+
+//Int para Float
+let idade = 20;
+console.log(parseFloat(idade));
+
+//Convertendo para String
+let num1 = 30.584;
+console.log(num1.toString());
+
+//Saber o numero da variavel
+let num = 123.456;
+let numx = num.toString();
+let verificar = typeof numx;
+console.log(verificar);
