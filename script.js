e1fd0ace56b17e6da84b5455763a585a94683fe7
Pablo <pabloallmeida123@gmail.com>
2024-03-21 09:29:22 -0300

    Fazendo manipulações em Strings

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ let num = 123.456;
 let numx = num.toString();
 let verificar = typeof numx;
 console.log(verificar);
+
+//Manipulaçcao com metodos
+
+//indexOf() - Mostra a posição de um trecho na String
+let texto = "Isso é um texto";
+console.log(texto.indexOf("é"))
+console.log(texto[5])
+
+//lastindexOf() - Faz a contagem do último
+let texto2 = "Textos são compostos por palavras";
+console.log(texto2.lastIndexOf("por"))
+
+//slice() - Mostra o recorte de um texto, com o início e fim dos caracteres 
+let texto3 = "Textos passam ideias";
+console.log(texto3.slice(texto3.indexOf("pa"), texto.indexOf("ia"))) //Usando uma função dentro da outra
+
+// length - Mostra quantos caracteres tem a string
+let texto4 = "Ideias transformam o mundo"
+console.log(texto4.length)
+alert(texto4.length)
+document.write(texto4.length)
